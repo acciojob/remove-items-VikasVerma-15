85c85b6244f487b4ab93da61cab4d140789d9765
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2026-01-02 15:49:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const button = document.querySelector('input[type="button"]');
+
+button.addEventListener("click", function () {
+  const select = document.getElementById("colorSelect");
+
+  // Remove the selected option
+  if (select.selectedIndex !== -1) {
+    select.remove(select.selectedIndex);
+  }
+});
